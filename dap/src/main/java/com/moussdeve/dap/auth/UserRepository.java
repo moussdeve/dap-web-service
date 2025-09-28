9812b1a0c07f3c0d91658d8338e87c99edbbd725
Armand Moussaouyi <moussdeve@gmail.com>
2025-09-28 11:45:10 -0500

    Adding auth user entity and Repository

--- dap/src/main/java/com/moussdeve/dap/auth/UserRepository.java
+++ dap/src/main/java/com/moussdeve/dap/auth/UserRepository.java
@@ -1,5 +1,14 @@
 package com.moussdeve.dap.auth;
 
-public interface UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long>{
+    Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
